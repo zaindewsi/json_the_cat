1839b53efce5aab3dd022fd5efce4cbd7597f0ee
Zain Dewsi <zain.dewsi@gmail.com>
2021-05-06 16:36:05 +0000

    added mocha chai test

---
+++ test/breedTest.js
@@ -0,0 +1,17 @@
+const { fetchBreed } = require("../breedFetcher");
+const { assert } = require("chai");
+
+describe("fetchBreedDescription", () => {
+  it("returns a string description for a valid breed, via callback", (done) => {
+    fetchBreed("Siberian", (err, desc) => {
+      assert.equal(err, null);
+
+      const expectedDesc =
+        "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+});
